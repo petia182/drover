04d171dcb14a7d5b7efe6d813847ed29d98eba55
Petya Nikolova <petia_182@yahoo.com>
2018-06-05 23:32:00 +0100

    adding components folder; deleting logo

---
+++ src/components/car.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Car extends React.Component {
+
+  render() {
+    return(
+      <div className="single-car">
+        <div className="car-image"></div>
+        <div className="car-description">Car Dscription here</div>
+      </div>
+    )
+  }
+}
+
+export default Car;
